0a319ccdbca37c0e9f3fce4ecf4b66edc891328e
Bharathimula <bharathim273@gmail.com>
2025-08-10 10:39:04 +0530

    modified

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('doctor/blog/create/', views.create_blog_post, name='create_blog_post'),
     path('doctor/blog/posts/', views.doctor_blog_posts, name='doctor_blog_posts'),
     path('blogs/', views.patient_blog_list, name='patient_blog_list'),
-    path('blogs/category/<int:category_id>/', views.patient_blog_list, name='patient_blog_list_by_category'),
+    path('blogs/category/<int:category_id>/', views.patient_blog_list_by_category, name='patient_blog_list_by_category'),
 ]
 
