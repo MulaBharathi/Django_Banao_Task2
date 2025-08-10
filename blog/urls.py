edb234b49950de298fc3cf02c36e4661a8d66ec2
Bharathimula <bharathim273@gmail.com>
2025-08-10 10:43:40 +0530

    modified

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('doctor/blog/posts/', views.doctor_blog_posts, name='doctor_blog_posts'),
     path('blogs/', views.patient_blog_list, name='patient_blog_list'),
     path('blogs/category/<int:category_id>/', views.patient_blog_list, name='patient_blog_list_by_category'),
-]i
+]
 
