0c2ed65e0fa1aa048f364de2066cbd5ed8666704
Bharathimula <bharathim273@gmail.com>
2025-08-10 10:28:18 +0530

    modofied

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_Banao.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
